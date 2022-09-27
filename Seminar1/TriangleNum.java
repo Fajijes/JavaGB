ffe84a996aba02dee401c9917322639f62c8fd73
Nikita <parnikaao@mail.ru>
2022-09-27 21:21:38 +0700

    update readme and folder seminar1

--- Seminar1/TriangleNum.java
+++ Seminar1/TriangleNum.java
@@ -1,13 +1,12 @@
 import java.io.IOException;
 
 public class TriangleNum {
-  static int calc (int n) {
-    System.out.println("Input number is "+ n);
-    return (n * (n + 1))/2;
+  static int calc(int n) {
+    System.out.println("Input number is " + n);
+    return (n * (n + 1)) / 2;
   }
 
   public static void main(String[] args) throws IOException {
     System.out.println("triangel number = " + TriangleNum.calc(2));
-  } 
+  }
 }
-      
